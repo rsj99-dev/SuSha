ae94e7d768ebc17745b3e580c3013ef5a49bcbf7
Shijie Ren <rsj99@126.com>
2026-05-11 19:40:10 +0800

    Update author and add license to setup.py
    Updated author name and added license information.

--- setup.py
+++ setup.py
@@ -3,8 +3,9 @@ from setuptools import setup, find_packages
 setup(
     name="SuSha",
     version="1.0.0",
-    author="Your Name",
+    author="rsj99",
     description="A multimodal ensemble learning prediction tool for microbial salinity adaptation based on genome-wide amino acid composition features.",
+    license="MIT",
     packages=find_packages(),
     include_package_data=True,
     package_data={
