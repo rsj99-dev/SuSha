3b5198343f3f3fe7d55cb8609c6aa85c5aa26969
Shijie Ren <rsj99@126.com>
2026-05-12 09:38:31 +0800

    Change version number to 0.1.0
    Updated version number from 1.0.0 to 0.1.0 in setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="SuSha",
-    version="1.0.0",
+    version="0.1.0",
     author="rsj99",
     description="A multimodal ensemble learning prediction tool for microbial salinity adaptation based on genome-wide amino acid composition features.",
     license="MIT",
